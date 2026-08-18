fc5baff98bb36fae40fe2de5d438aaf33c5d13c7
Kodama <aaronbrown.personal@pm.me>
2026-08-17 21:42:43 -0500

    chore(NovelArchive): bump to 1.0.0-alpha.2

--- src/NovelArchive/pbconfig.ts
+++ src/NovelArchive/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "NovelArchive",
   description: "Read web novels from novelarchive.cc.",
-  version: "1.0.0-alpha.1",
+  version: "1.0.0-alpha.2",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
