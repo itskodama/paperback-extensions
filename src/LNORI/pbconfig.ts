9afd796979e50386a062ad6ec780cd2d18cf808e
Kodama <aaronbrown.personal@pm.me>
2026-08-17 21:42:32 -0500

    chore(LNORI): bump to 1.0.0-alpha.4

--- src/LNORI/pbconfig.ts
+++ src/LNORI/pbconfig.ts
@@ -7,7 +7,7 @@ export default {
   name: "LNORI",
   description:
     "Read light novels from lnori.com. Enable 'Chapters Unique by Volume' under Manage Version Priority for correct chapter lists.",
-  version: "1.0.0-alpha.3",
+  version: "1.0.0-alpha.4",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
