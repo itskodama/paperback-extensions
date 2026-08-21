e6cf339e70da6897ba1dcf99a912e3019331ab5c
Kodama <aaronbrown.personal@pm.me>
2026-08-21 16:33:10 -0500

    chore(FreeWebNovel): bump to 1.0.0-alpha.4

--- src/FreeWebNovel/pbconfig.ts
+++ src/FreeWebNovel/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "FreeWebNovel",
   description: "Read web novels from freewebnovel.com.",
-  version: "1.0.0-alpha.3",
+  version: "1.0.0-alpha.4",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
